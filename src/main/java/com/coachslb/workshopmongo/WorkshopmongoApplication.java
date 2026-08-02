8111b41ba7113562d8a12d698f902895105c80ef
José Garção <josegarcao1@gmail.com>
2026-08-02 10:33:15 +0100

    Entity User + UserController

--- src/main/java/com/coachslb/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/coachslb/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.coachsl.workshopmongo;
+package com.coachslb.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
